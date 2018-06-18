473abf7c2546dc248c3d925ece3fe2dfc61baaa1
Matthias Dittrich <Matthias.Dittrich@aitgmbh.de>
2018-06-18 20:41:29 +0200

    some hacking commited to switch hardware

---
+++ demo/server-client/server.js
@@ -0,0 +1,12 @@
+"use strict";
+exports.__esModule = true;
+var express = require("express");
+var routes = require("./api/routes/componentsRoutes");
+var app = express();
+var port = process.env.PORT || 3000;
+app.listen(port);
+routes.startApp(app);
+app.use(function (req, res) {
+    res.status(404).send({ url: req.originalUrl + ' not found' });
+});
+console.log('todo list RESTful API server started on: ' + port);
